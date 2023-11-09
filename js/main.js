83b32fdd71e620ffc0c9416d21a2f32bfd1771f1
GonzaBottone <bottonegonza@gmail.com>
2023-11-09 20:31:07 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,98 +1,28 @@
-const productos = [
-    {
-      nombre: "medialunas",
-      precio: 100,
-    },
-    {
-      nombre: "galletitas",
-      precio: 60,
-    },
-    {
-      nombre: "chocolate",
-      precio: 150,
-    },
-    {
-      nombre: "palmeritas",
-      precio: 400,
-    },
-    {
-      nombre: "Bolas de fraile",
-      precio: 300,
-    },
-  ];
-  let carrito = [];
-  
-  
-  let seleccion = prompt("hola desea comprar algo si o no");
-  
-  while (seleccion != "si" && seleccion != "no") {
-    alert("por favor ingresa una de las opciones, si o no");
-    seleccion = prompt("hola desea comprar algo si o no");
-  }
-  
-  if (seleccion == "si") {
-    alert("a continuación lista de productos");
-    let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todosLosProductos.join(" - "));
-  } else if (seleccion == "no") {
-    alert("gracias por venir al supermecado! hasta luego!");
-  }
-  
-  
-  while (seleccion != "no") {
-    let producto = prompt("agrega un producto a tu carrito!");
-    let precio = 0;
-  
-    if (
-      producto == "medialunas" ||
-      producto == "galletitas" ||
-      producto == "chocolate" ||
-      producto == "palmeritas" ||
-      producto == "Bolas de fraile"
-    ) {
-   
-      switch (producto) {
-        case "medialunas":
-          precio = 50;
-          break;
-        case "galletitas":
-          precio = 100;
-          break;
-        case "chocolate":
-          precio = 150;
-          break;
-        case "palmeritas":
-          precio = 400;
-          break;
-        case "Bolas de fraile":
-          precio = 500;
-          break;
-        default:
-          break;
-      }
-      let unidades = parseInt(prompt("cuantas unidades de ese producto quieres llevar"));
-  
-      carrito.push({ producto, unidades, precio });
-      console.log(carrito);
-    } else {
-      alert("no tenemos ese prodcto");
-    }
-  
-    
-   seleccion = prompt("desea seguir comprando si o no");
-  
-  
-   carrito.forEach((carritoFinal) =>  {
-        console.log(
-          `"producto:" ${carritoFinal.producto}, "unidades:" ${carritoFinal.unidades}, "total a pagar por producto:" ${
-            carritoFinal.unidades * carritoFinal.precio
-          }`
-        );
-      });
-      break;
-    }
+function limpiar() {
+  document.getElementById('miFormulario').reset();
+}
 
-  
-  
-  const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-  console.log(`el total a pagar por su compra es de:  ${total}`);  
+function sumar() {
+  var x = parseInt(document.getElementById('valor1').value)
+  var y = parseInt(document.getElementById('valor2').value)
+  document.getElementById('el-resultado').innerHTML = x+y
+}
+
+function restar() {
+  var x = parseInt(document.getElementById('valor1').value)
+  var y = parseInt(document.getElementById('valor2').value)
+  document.getElementById('el-resultado').innerHTML = x-y
+}
+
+function multiplicar() {
+  var x = parseInt(document.getElementById('valor1').value)
+  var y = parseInt(document.getElementById('valor2').value)
+  document.getElementById('el-resultado').innerHTML = x*y
+}
+
+function dividir() {
+  var x = parseInt(document.getElementById('valor1').value)
+  var y = parseInt(document.getElementById('valor2').value)
+  document.getElementById('el-resultado').innerHTML = x/y
+}
+
